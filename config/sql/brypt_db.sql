87f6692f237e1daaf5f0549a042f32c1adef82a7
Terri Hewitt <hewittt@oregonstate.edu>
2018-10-25 13:33:39 -0700

    Added users and users_networks tables

--- config/sql/brypt_db.sql
+++ config/sql/brypt_db.sql
@@ -1,16 +1,40 @@
---Add create table methods here
+DROP TABLE IF EXISTS `brypt_users`;
+
+CREATE TABLE `brypt_users` (
+	`user_id` char(32) NOT NULL,
+	`username` varchar(32) NOT NULL,
+	`first_name` varchar(32) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`organization` varchar(255),
+	`age` datetime,
+	`join_date` datetime,
+	`last_login` datetime,
+	`login_attempts` int(11),
+	`login_token` char(255),
+	`region` char(255),
+	PRIMARY KEY (`user_id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 DROP TABLE IF EXISTS `brypt_nodes`;
 
 CREATE TABLE `brypt_nodes` (
-	`id` varchar(32) NOT NULL,
-	`serial_number` varchar(255) NOT NULL,
+	`node_id` char(32) NOT NULL,
+	`serial_number` char(255) NOT NULL,
 	`type` varchar(32),
 	`created_on` datetime,
 	`registered_on` datetime,
-	`registered_to` varchar(32),
-	`connected_network`varchar(32),
-	--TODO: Add foreign key constraints once other tables created
-	PRIMARY KEY (`id`)
+	`registered_to` char(32),
+	`connected_network`char(32),
+	PRIMARY KEY (`node_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---MongoDB???
+
+DROP TABLE IF EXISTS `brypt_users_networks`;
+
+CREATE TABLE `brypt_users_networks` (
+	`user_id` char(32) NOT NULL,
+	`network_id` char(32) NOT NULL,
+	PRIMARY KEY (`user_id`, `network_id`),
+	CONSTRAINT `users_networks_fk1` FOREIGN KEY (`user_id`) REFERENCES `brypt_users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
