df76fd16cf979a59c2b8ffc6f73147b239c14f0e
Terri Hewitt <hewittt@oregonstate.edu>
2018-11-18 18:27:15 -0800

    Created outline of collections to create in mongodb

---
+++ config/mongodb/mongo_scripts.go
@@ -0,0 +1,28 @@
+// Switch to database (or create it)
+
+
+// Create collection called "brypt_users"
+
+
+// Create a collection called "brypt_nodes"
+
+
+// Create a collection "brypt_users_networks" for many:many relationship???
+
+
+// Create a collection called "brypt_networks"
+
+
+// Add a network to the collection (insert data into collection)
+
+
+// Remove a network from the collection
+
+
+// Retrieve a network ID based on the network name (network name must be unique for this to work!)
+
+
+// Update a network in the collection .... maybe wait on this
+
+
+
