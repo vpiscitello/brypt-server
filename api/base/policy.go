f084bc2d7684747f8c8bcd6a8590d6e559ffae43
Adam Barton <adamb6102@gmail.com>
2018-12-21 21:42:31 -0800

    Added new files necessary for terms and conditions and privacy policy, accessible currently through the /policy page

--- api/base/policy.go
+++ api/base/policy.go
@@ -1,5 +1,23 @@
 package base
 
+import (
+	"net/http"
+
+	"brypt-server/internal/handlebars"
+)
+
 // privacy policy
+func RenderPolicy(w http.ResponseWriter, r *http.Request) {
+
+	policyCTX := make(map[string]interface{})
+
+	policyCTX["title"] = "Brypt"
+
+	page := handlebars.CompilePage( "policy", policyCTX )
+
+	w.Header().Set( "Content-Type", "text/html" )
+	w.Write( []byte( page ) )
+
+}
 
 // terms of service
